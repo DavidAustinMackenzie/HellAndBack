cce6443cb35f0b2915583b7760ca3aceb3ac00b6
David Mackenzie <dmackenzie1985@gmail.com>
2025-04-25 21:05:49 +1000

    Add project files.

---
+++ HellAndBack/Models/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace HellAndBack.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public int Wins { get; set; }
+
+        public int Loses { get; set; }
+
+    }
+}
