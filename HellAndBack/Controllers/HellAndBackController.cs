dd4e362eda1232ccbde29253d67a4f884688292a
David Mackenzie <dmackenzie1985@gmail.com>
2025-04-25 21:58:57 +1000

    Added HAB Contrller and View

---
+++ HellAndBack/Controllers/HellAndBackController.cs
@@ -0,0 +1,20 @@
+﻿using HellAndBack.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HellAndBack.Controllers
+{
+    public class HellAndBackController : Controller
+    {
+        public IActionResult Index()
+        {
+            var cards = new List<Card>();
+            Card card = new Card();
+            card.CardId = 1;
+            card.Value = "K";
+            card.Suit = "♣";
+            cards.Add(card);
+            ViewData["cards"] = cards;
+            return View();
+        }
+    }
+}
