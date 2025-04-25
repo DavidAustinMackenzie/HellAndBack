cd2377185c459c978129c77660fa4a6b4d3a27a6
David Mackenzie <dmackenzie1985@gmail.com>
2025-04-25 21:13:14 +1000

    Added Player and Card Models

---
+++ HellAndBack/Models/Card.cs
@@ -0,0 +1,11 @@
+﻿namespace HellAndBack.Models
+{
+    public class Card
+    {
+        public int CardId { get; set; }
+
+        public string Value { get; set; }
+
+        public string Suit {  get; set; }
+    }
+}
--- HellAndBack/Models/Player.cs
+++ HellAndBack/Models/Player.cs
@@ -1,12 +1,17 @@
-﻿namespace HellAndBack.Models
+﻿using System.Collections.ObjectModel;
+
+namespace HellAndBack.Models
 {
     public class Player
     {
+        public int PlayerId { get; set; }
+
         public string Name { get; set; }
 
         public int Wins { get; set; }
 
         public int Loses { get; set; }
 
+        public ObservableCollection<Card> Hand {  get; set; }
     }
 }
